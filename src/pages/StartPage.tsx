a1cddf22b935895926de0820878801de3c102beb
mishamin116@gmail.com <mishamin116@gmail.com>
2020-04-27 13:44:43 +0700

    startPagecorrect

--- src/pages/StartPage.tsx
+++ src/pages/StartPage.tsx
@@ -1,19 +1,9 @@
 import React from "react";
 import { observer } from "mobx-react";
-import { useHistory } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
-import { Container, CssBaseline } from "@material-ui/core";
-import Avatar from "@material-ui/core/Avatar";
-import Alert from "@material-ui/lab/Alert";
-import Button from "@material-ui/core/Button";
-import TextField from "@material-ui/core/TextField";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Checkbox from "@material-ui/core/Checkbox";
+import { Container } from "@material-ui/core";
 import Link from "@material-ui/core/Link";
-import Grid from "@material-ui/core/Grid";
-import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import Typography from "@material-ui/core/Typography";
-import { useStores } from "../hooks/strores.hook";
 
 const useStyles = makeStyles((theme) => ({
   startpage: {
@@ -23,8 +13,6 @@ const useStyles = makeStyles((theme) => ({
 
 export const StartPage: React.FC = observer(() => {
   const classes = useStyles();
-
-  const preventDefault = (e: { preventDefault: () => any }) => e.preventDefault();
 
   return (
     <Container className={classes.startpage} component="main" maxWidth="sm">
