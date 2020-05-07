3decf231243794163f8b5daf95aa14334a9f3f04
mishamin116@gmail.com <mishamin116@gmail.com>
2020-05-07 20:19:05 +0700

    baseurl

--- src/stores/apiClient.ts
+++ src/stores/apiClient.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  // baseURL: 'https://api.bimgen.org',
-  baseURL: 'http://localhost:3000',
+  baseURL: 'https://api.bimgen.org',
+  // baseURL: 'http://localhost:3000',
 })
 
 export default apiClient
